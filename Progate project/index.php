4e65747e6a6896ce4c79eda9bda81ed9dd8915ae
Tu Ngo <tu.ngo@ntq-solution.com.vn>
2020-03-16 17:39:24 +0700

    16/3

---
+++ Progate project/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Progate</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+<body>
+<?php
+
+echo 'Hello, PHP';
+echo '<br>';
+echo '10 + 7';
+
+?>
+</body>
+</html>
+
